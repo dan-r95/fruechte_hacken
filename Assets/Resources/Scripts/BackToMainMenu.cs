437428a911f69380fc258c7f9db42a28d6b80d2f
dan_r95 <d.rossburg@googlemail.com>
2019-08-20 19:11:06 +0200

    sparkles for bomb

---
+++ Assets/Resources/Scripts/BackToMainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Player")
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+        
+    }
+}
